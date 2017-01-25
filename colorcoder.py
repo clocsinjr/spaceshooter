f49cc8d572b032b1c9087ccea2692114a12b9a58
Carlo Locsin jr <ignidatio@gmail.com>
2017-01-25 16:38:51 +0100

    updated events to add grunts in random spawns

--- colorcoder.py
+++ colorcoder.py
@@ -28,7 +28,7 @@ CYAN = 52223
 pygame.init()
 
 # Set the width and height of the screen [width, height]
-size = (600, 920)
+size = (int(600 * 0.8), int(920 * 0.8))
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Cloc's Spacex")
 
@@ -40,12 +40,12 @@ inmenu = False
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
-#set buttonrepeating on
+# set buttonrepeating on
 pygame.key.set_repeat(1, 10)
 
 pygame.mouse.set_visible(False)
 
-#original gamestate:
+# original gamestate:
 playerGroup = pygame.sprite.Group()
 pickups = pygame.sprite.Group()
 fProj = pygame.sprite.Group()
@@ -56,312 +56,314 @@ player = entity.player(size)
 player.add(playerGroup)
 
 
-
 pygame.font.init()
 scorefont = pygame.font.Font("vgasys.fon", 64)
 
-scorex = (size[0] / 7) * 5 
+scorex = (size[0] / 7) * 5
 scorey = size[1] - 110
 
-hpx = (size[0] / 7) * 6 
+hpx = (size[0] / 7) * 6
 hpy = size[1] - 110
 
 diffx = (size[0] / 7) * 3
-diffy =  size[1] - 110
+diffy = size[1] - 110
 
 hpbarxmin = 50
 hpbarxmax = size[0] - 50
 hpbarymax = size[1] - 50
 hpbarymin = hpbarymax - 30
-hpbarlen = hpbarxmax - hpbarxmin 
+hpbarlen = hpbarxmax - hpbarxmin
 
 
 time = 0
-difficulty = 1.0
+difficulty = 1.4
 diffcount = 1
 
 
 def drawEntities():
-	# draw player
-	playerGroup.draw(screen)
-	
-	# draw enemies
-	enemies.draw(screen)
-	
-	# draw projectiles
-	fProj.draw(screen)
-	eProj.draw(screen)
-	
-	pickups.draw(screen)
-	
-	if player.scopeTime > 0 and player.alive:
-		start_pos = (player.rect[0] + entity.PSIZE, player.rect[1])
-		end_pos = (player.rect[0] + entity.PSIZE, 0)
-		pygame.draw.line(screen, CYAN, start_pos, end_pos, 1)
+    # draw player
+    playerGroup.draw(screen)
+
+    # draw enemies
+    enemies.draw(screen)
+
+    # draw projectiles
+    fProj.draw(screen)
+    eProj.draw(screen)
+
+    pickups.draw(screen)
+
+    if player.scopeTime > 0 and player.alive:
+        start_pos = (player.rect[0] + entity.PSIZE, player.rect[1])
+        end_pos = (player.rect[0] + entity.PSIZE, 0)
+        pygame.draw.line(screen, CYAN, start_pos, end_pos, 1)
+
 
 def drawgui():
-	scoretxt = scorefont.render("Score: " + str(player.score), False, WHITE)
-	screen.blit(scoretxt, (scorex, scorey))
-	
-	hptxt = scorefont.render("HP: " + str(player.HP) + "/ 100", False, WHITE)
-	screen.blit(hptxt, (hpx, hpy))
-	
-	difftext = scorefont.render("Difficulty: " + str(difficulty), False, WHITE)
-	screen.blit(difftext, (diffx, diffy))
-	
-	hpbarlim = hpbarlen * (player.HP/ 100.0) + 1
-	hpbarlim2 = hpbarlen * ((100 - player.HP )/ 100.0)
-	
-	if player.HP > 0.0:
-		pygame.draw.rect(screen, GREEN, [hpbarxmin, hpbarymin, hpbarlim, 30])
-	
-	if player.HP < 100.0:
-		pygame.draw.rect(screen, RED, [hpbarlim + hpbarxmin, hpbarymin, hpbarlim2, 30])
+    scoretxt = scorefont.render("Score: " + str(player.score), False, WHITE)
+    screen.blit(scoretxt, (scorex, scorey))
+
+    hptxt = scorefont.render("HP: " + str(player.HP) + "/ 100", False, WHITE)
+    screen.blit(hptxt, (hpx, hpy))
+
+    difftext = scorefont.render("Difficulty: " + str(difficulty), False, WHITE)
+    screen.blit(difftext, (diffx, diffy))
+
+    hpbarlim = hpbarlen * (player.HP / 100.0) + 1
+    hpbarlim2 = hpbarlen * ((100 - player.HP) / 100.0)
+
+    if player.HP > 0.0:
+        pygame.draw.rect(screen, GREEN, [hpbarxmin, hpbarymin, hpbarlim, 30])
+
+    if player.HP < 100.0:
+        pygame.draw.rect(
+            screen, RED, [hpbarlim + hpbarxmin, hpbarymin, hpbarlim2, 30])
 
 
 def spawnEnemy(type, x):
-	enemy = None
-	if type == events.EVENT_FODDER:
-		enemy = entity.fodder(size, x, 0)
-	elif type == events.EVENT_SINE:
-		enemy = entity.sine(size, x, 0)
-	elif type == events.EVENT_GRUNT:
-		enemy = entity.grunt(size, x, 0)
-	elif type == events.EVENT_SENTRY:
-		enemy = entity.sentry(size, x, 0)
-	
-	enemy.add(enemies)
+    enemy = None
+    if type == events.EVENT_FODDER:
+        enemy = entity.fodder(size, x, 0)
+    elif type == events.EVENT_SINE:
+        enemy = entity.sine(size, x, 0)
+    elif type == events.EVENT_GRUNT:
+        enemy = entity.grunt(size, x, 0)
+    elif type == events.EVENT_SENTRY:
+        enemy = entity.sentry(size, x, 0)
+
+    enemy.add(enemies)
 
 
 def spawnBuff(type, x):
-	buff = None
-	if type == events.EVENT_HPUP:
-		buff = entity.hpup(size, x, 0)
-	elif type == events.EVENT_SCOPE:
-		buff = entity.scope(size, x, 0)
-	elif type == events.EVENT_AS:
-		buff = entity.AS(size, x, 0)
-	buff.add(pickups)
-	
+    buff = None
+    if type == events.EVENT_HPUP:
+        buff = entity.hpup(size, x, 0)
+    elif type == events.EVENT_SCOPE:
+        buff = entity.scope(size, x, 0)
+    elif type == events.EVENT_AS:
+        buff = entity.AS(size, x, 0)
+    buff.add(pickups)
+
+
 def handleEvents():
-	# add events to the event queues
-	events.addEvents(size, difficulty)
-	
-	# check the enemy event queues for tasks and execute them
-	for eventType in events.enemySpawn:
-		if len(events.enemySpawn[eventType]) == 0:
-			continue
-		
-		# get coordinates list for this task type
-		coords = events.enemySpawn[eventType].pop()
-		
-		for x in coords:
-			if x is not None:
-				spawnEnemy(eventType, x)
-	
-	# check the buffs event queues for tasks
-	for eventType in events.buffSpawn:
-		if len(events.buffSpawn[eventType]) == 0:
-			continue
-		coords = events.buffSpawn[eventType].pop()
-		for x in coords:
-			if x is not None:
-				spawnBuff(eventType, x)
-	
-	
+    # add events to the event queues
+    events.addEvents(size, difficulty)
+
+    # check the enemy event queues for tasks and execute them
+    for eventType in events.enemySpawn:
+        if len(events.enemySpawn[eventType]) == 0:
+            continue
+
+        # get coordinates list for this task type
+        coords = events.enemySpawn[eventType].pop()
+
+        for x in coords:
+            if x is not None:
+                spawnEnemy(eventType, x)
+
+    # check the buffs event queues for tasks
+    for eventType in events.buffSpawn:
+        if len(events.buffSpawn[eventType]) == 0:
+            continue
+        coords = events.buffSpawn[eventType].pop()
+        for x in coords:
+            if x is not None:
+                spawnBuff(eventType, x)
+
+
 """ Check for collisions with projectiles. """
+
+
 def checkCollisions():
-	playerhit = False
-	# check for collisions between friendly projectiles and enemies
-	coldict = pygame.sprite.groupcollide(enemies, fProj, False, True)
-	
-	# keys: enemies hit, values: lists of intersecting projectiles
-	if coldict != {}:
-		sounds.hitMarker.play(sounds.hitEnemySound)
-		
-		# check for all projectiles and enemies for collisions
-		for enemyHit in coldict:
-			for projs in coldict[enemyHit]:
-			
-				# play sounds and adjust stats based on projectiles and Hp
-				enemyHit.HP -= projs.DMG
-				if enemyHit.HP <= 0.0:
-					sounds.hitMarker.play(sounds.killEnemySound)
-					enemyHit.kill()
-					player.score += enemyHit.points
-	
-	# check enemy projectile collisions with player
-	coldict = pygame.sprite.groupcollide(playerGroup, eProj, False, True)
-	if coldict != {}:
-		sounds.playerHit.play(sounds.hitPlayerSound)
-		for projs in coldict[player]:
-			player.HP -= projs.DMG
-			playerhit = True
-	
-	# check enemy entity collisions with player
-	coldict = pygame.sprite.groupcollide(playerGroup, enemies, False, True)
-	if coldict != {}:
-		sounds.hitMarker.play(sounds.hitEnemySound)
-		for enemy in coldict[player]:
-			player.HP -= 10
-			playerhit = True
-	
-	# check pickup collisions with player
-	coldict = pygame.sprite.groupcollide(playerGroup, pickups, False, True)
-	if coldict != {}:
-		sounds.pickups.play(sounds.pickupBuff)
-		for buff in coldict[player]:
-			if isinstance(buff, entity.hpup):
-				player.HP += buff.HP
-				if player.HP > 100:
-					player.HP = 100
-			
-			if isinstance(buff, entity.scope):
-				player.scopeTime = buff.time
-			
-			if isinstance(buff, entity.AS):
-				player.ASTime = buff.time
-				player.gunCD = 5.0
-	
-	# if the player got a killing blow, play a sound and off the player
-	if player.HP <= 0.0 and playerhit == True:
-		sounds.hitMarker.play(sounds.killEnemySound)
-		player.alive = False
-		player.kill()
-		
+    playerhit = False
+    # check for collisions between friendly projectiles and enemies
+    coldict = pygame.sprite.groupcollide(enemies, fProj, False, True)
+
+    # keys: enemies hit, values: lists of intersecting projectiles
+    if coldict != {}:
+        sounds.hitMarker.play(sounds.hitEnemySound)
+
+        # check for all projectiles and enemies for collisions
+        for enemyHit in coldict:
+            for projs in coldict[enemyHit]:
+
+                # play sounds and adjust stats based on projectiles and Hp
+                enemyHit.HP -= projs.DMG
+                if enemyHit.HP <= 0.0:
+                    sounds.hitMarker.play(sounds.killEnemySound)
+                    enemyHit.kill()
+                    player.score += enemyHit.points
+
+    # check enemy projectile collisions with player
+    coldict = pygame.sprite.groupcollide(playerGroup, eProj, False, True)
+    if coldict != {}:
+        sounds.playerHit.play(sounds.hitPlayerSound)
+        for projs in coldict[player]:
+            player.HP -= projs.DMG
+            playerhit = True
+
+    # check enemy entity collisions with player
+    coldict = pygame.sprite.groupcollide(playerGroup, enemies, False, True)
+    if coldict != {}:
+        sounds.hitMarker.play(sounds.hitEnemySound)
+        for enemy in coldict[player]:
+            player.HP -= 10
+            playerhit = True
+
+    # check pickup collisions with player
+    coldict = pygame.sprite.groupcollide(playerGroup, pickups, False, True)
+    if coldict != {}:
+        sounds.pickups.play(sounds.pickupBuff)
+        for buff in coldict[player]:
+            if isinstance(buff, entity.hpup):
+                player.HP += buff.HP
+                if player.HP > 100:
+                    player.HP = 100
+
+            if isinstance(buff, entity.scope):
+                player.scopeTime = buff.time
+
+            if isinstance(buff, entity.AS):
+                player.ASTime = buff.time
+                player.gunCD = 5.0
+
+    # if the player got a killing blow, play a sound and off the player
+    if player.HP <= 0.0 and playerhit == True:
+        sounds.hitMarker.play(sounds.killEnemySound)
+        player.alive = False
+        player.kill()
+
 
 def updatePlayer():
-	# check pressed keys
-	left = keys[pygame.K_LEFT]
-	right = keys[pygame.K_RIGHT]
-	fire = keys[pygame.K_SPACE]
-	
-	# move player based on keys
-	if left and not right:
-		player.move(entity.PDIRL, size)
-	elif right and not left:
-		player.move(entity.PDIRR, size)
-	if not left and not right:
-		player.move(entity.PDIRN, size)
-	
-	# fire projectiles based on key
-	if fire and player.gunTime == 0:
-		sounds.fireChannel.play(sounds.playerFireSound)
-		entity.projectile(player).add(fProj)
-		player.gunTime = player.gunCD
-	
-	# check AS buff
-	if player.ASTime == 0:
-		player.gunCD = 10
-	player.updateTimers()
-	
+    # check pressed keys
+    left = keys[pygame.K_LEFT]
+    right = keys[pygame.K_RIGHT]
+    fire = keys[pygame.K_SPACE]
+
+    # move player based on keys
+    if left and not right:
+        player.move(entity.PDIRL, size)
+    elif right and not left:
+        player.move(entity.PDIRR, size)
+    if not left and not right:
+        player.move(entity.PDIRN, size)
+
+    # fire projectiles based on key
+    if fire and player.gunTime == 0:
+        sounds.fireChannel.play(sounds.playerFireSound)
+        entity.projectile(player).add(fProj)
+        player.gunTime = player.gunCD
+
+    # check AS buff
+    if player.ASTime == 0:
+        player.gunCD = 10
+    player.updateTimers()
+
+
 def updateEnemies():
-	for enm in enemies:
-		enm.move()
-		
-		isGrunt = isinstance(enm, entity.grunt)
-		isSentry = isinstance(enm, entity.sentry)
-		
-		if enm.rect[1] > size[1]:
-			enm.kill()
-		if isGrunt or isSentry:
-			enm.gunTime -= 1
-			if enm.gunTime <= 0:
-				sounds.fireChannel.play(sounds.playerFireSound)
-				entity.projectile(enm).add(eProj)
-				enm.gunTime = enm.gunCD
-		
-		if isSentry:
-			enm.detFacing(player)
-		
+    for enm in enemies:
+        enm.move()
+
+        isGrunt = isinstance(enm, entity.grunt)
+        isSentry = isinstance(enm, entity.sentry)
+
+        if enm.rect[1] > size[1]:
+            enm.kill()
+        if isGrunt or isSentry:
+            enm.gunTime -= 1
+            if enm.gunTime <= 0:
+                sounds.fireChannel.play(sounds.playerFireSound)
+                entity.projectile(enm).add(eProj)
+                enm.gunTime = enm.gunCD
+
+        if isSentry:
+            enm.detFacing(player)
 
 
 def updatePickups():
-	for pickup in pickups:
-		pickup.move()
-		
-		if pickup.rect[1] > size[1]:
-			pickup.kill()
+    for pickup in pickups:
+        pickup.move()
+
+        if pickup.rect[1] > size[1]:
+            pickup.kill()
+
 
 def updateProjectiles():
-	# move projectiles
-	for projectile in fProj.sprites():
-		projectile.moveProjectile()
-	for projectile in eProj.sprites():
-		projectile.moveProjectile()
-	
-	# check if friendly projectiles are out of bounds
-	for projectile in fProj.sprites():
-		x = projectile.rect[0]
-		y = projectile.rect[1]
-		if ((x < 0 or x > size[0]) or (y < 0 or y > size[1])):
-			projectile.kill()
-	
-	# check if enemy projectiles are out of bounds
-	for projectile in eProj.sprites():
-		x = projectile.rect[0]
-		y = projectile.rect[1]
-		if ((x < 0 or x > size[0]) or (y < 0 or y > size[1])):
-			projectile.kill()
+    # move projectiles
+    for projectile in fProj.sprites():
+        projectile.moveProjectile()
+    for projectile in eProj.sprites():
+        projectile.moveProjectile()
 
+    # check if friendly projectiles are out of bounds
+    for projectile in fProj.sprites():
+        x = projectile.rect[0]
+        y = projectile.rect[1]
+        if ((x < 0 or x > size[0]) or (y < 0 or y > size[1])):
+            projectile.kill()
+
+    # check if enemy projectiles are out of bounds
+    for projectile in eProj.sprites():
+        x = projectile.rect[0]
+        y = projectile.rect[1]
+        if ((x < 0 or x > size[0]) or (y < 0 or y > size[1])):
+            projectile.kill()
 
 
 # -------- Main Program Loop -----------
 if __name__ == '__main__':
-	while not done:
-		
-		#general input:
-		keys = pygame.key.get_pressed()
-		
-		# --- Main event loop
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				done = True
-		
-			if event.type == pygame.KEYUP:
-				if event.key == pygame.K_ESCAPE:
-					pygame.event.clear(pygame.KEYUP)
-					paused = not paused
-		
-		# in-game
-		if paused != True:
-			# --- Game logic should go here
-			if player.alive:
-				updatePlayer()
-			updateEnemies()
-			updatePickups()
-			updateProjectiles()
-			
-			checkCollisions()
-			
-			if time % 16 == 0: handleEvents()
-			
-			if (player.score / 500.0) >= diffcount:
-				diffcount += 1
-				difficulty *= 1.2
-		
-			
-			# --- Drawing code should go here
-			screen.fill(131094)
-			
-			drawEntities()
-			drawgui()
-			
-			# --- tick timers
-			
+    while not done:
 
-		# when paused:
-		else:
-			pass
-			
-		""" === end else ========================================================== """
-		
-		#flip screen
-		pygame.display.flip()
-		
-		time += 1
-		clock.tick(FPS)
-		
-	
-	# Close the window and quit.
-	pygame.quit()
+        # general input:
+        keys = pygame.key.get_pressed()
+
+        # --- Main event loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.event.clear(pygame.KEYUP)
+                    paused = not paused
+
+        # in-game
+        if paused != True:
+            # --- Game logic should go here
+            if player.alive:
+                updatePlayer()
+            updateEnemies()
+            updatePickups()
+            updateProjectiles()
+
+            checkCollisions()
+
+            if time % 16 == 0:
+                handleEvents()
+
+            if (player.score / 500.0) >= diffcount:
+                diffcount += 1
+                difficulty *= 1.4
+
+            # --- Drawing code should go here
+            screen.fill(131094)
+
+            drawEntities()
+            drawgui()
+
+            # --- tick timers
+
+        # when paused:
+        else:
+            pass
+
+        """ === end else ========================================================== """
+
+        # flip screen
+        pygame.display.flip()
+
+        time += 1
+        clock.tick(FPS)
+
+    # Close the window and quit.
+    pygame.quit()
--- events.py
+++ events.py
@@ -13,83 +13,101 @@ EVENT_HPUP = 5
 EVENT_SCOPE = 6
 EVENT_AS = 7
 
-enemySpawn = {EVENT_FODDER: [], EVENT_SINE: [], EVENT_GRUNT : [], EVENT_SENTRY : []}
+enemySpawn = {EVENT_FODDER: [], EVENT_SINE: [],
+              EVENT_GRUNT: [], EVENT_SENTRY: []}
 buffSpawn = {EVENT_HPUP: [], EVENT_SCOPE: [], EVENT_AS: []}
 
 
 def addEventFodder(size):
-	x = random.randint(entity.RFODDER, size[0] - entity.RFODDER)
-	enemySpawn[EVENT_FODDER].append([x])
+    x = random.randint(entity.RFODDER, size[0] - entity.RFODDER)
+    enemySpawn[EVENT_FODDER].append([x])
+
 
 def addEventSentry(size):
-	x = random.randint(entity.RSENTRY, size[0] - entity.RSENTRY)
-	enemySpawn[EVENT_SENTRY].append([x])
+    x = random.randint(entity.RSENTRY, size[0] - entity.RSENTRY)
+    enemySpawn[EVENT_SENTRY].append([x])
+
 
 def addEventSinewave(size):
-	# choose an originx that will fit the whole sine wave
-	x = random.randint(entity.RSINE + entity.SINEAMP, size[0] - entity.RSINE - entity.SINEAMP)
-	num = random.randint(5, 10)
-	thisEvent = []
-	for i in range(num):
-		thisEvent.append([x])
-	
-	enemySpawn[EVENT_SINE].extend(thisEvent)
+    # choose an originx that will fit the whole sine wave
+    x = random.randint(entity.RSINE + entity.SINEAMP,
+                       size[0] - entity.RSINE - entity.SINEAMP)
+    num = random.randint(5, 10)
+    thisEvent = []
+    for i in range(num):
+        thisEvent.append([x])
+
+    enemySpawn[EVENT_SINE].extend(thisEvent)
+
+
+def addEventGrunt(size):
+    x = random.randint(entity.RFODDER, size[0] - entity.RFODDER)
+    enemySpawn[EVENT_GRUNT].append([x])
+
 
 def addEventVgrunts(size):
-	# origin x to fit the whole V shape
-	x = random.uniform(entity.RGRUNT + 128, size[0] - (entity.RGRUNT + 128))
-	
-	thisEvent = []
-	thisEvent.append([(x - 128), (x + 128)])
-	thisEvent.append([None])
-	thisEvent.append([(x - 64), (x + 64)])
-	thisEvent.append([None])
-	thisEvent.append([x])
-	
-	enemySpawn[EVENT_GRUNT].extend(thisEvent)
+    # origin x to fit the whole V shape
+    x = random.uniform(entity.RGRUNT + 128, size[0] - (entity.RGRUNT + 128))
 
+    thisEvent = []
+    thisEvent.append([(x - 128), (x + 128)])
+    thisEvent.append([None])
+    thisEvent.append([(x - 64), (x + 64)])
+    thisEvent.append([None])
+    thisEvent.append([x])
+
+    enemySpawn[EVENT_GRUNT].extend(thisEvent)
 
 
 def addEventHPup(size):
-	x = random.randint(entity.RFODDER, size[0] - entity.RFODDER)
-	buffSpawn[EVENT_HPUP].append([x])
+    x = random.randint(entity.RFODDER, size[0] - entity.RFODDER)
+    buffSpawn[EVENT_HPUP].append([x])
+
 
 def addEventScope(size):
-	x = random.randint(entity.RFODDER, size[0] - entity.RFODDER)
-	buffSpawn[EVENT_SCOPE].append([x])
+    x = random.randint(entity.RFODDER, size[0] - entity.RFODDER)
+    buffSpawn[EVENT_SCOPE].append([x])
+
 
 def addEventAS(size):
-	x = random.randint(entity.RFODDER, size[0] - entity.RFODDER)
-	buffSpawn[EVENT_AS].append([x])
+    x = random.randint(entity.RFODDER, size[0] - entity.RFODDER)
+    buffSpawn[EVENT_AS].append([x])
+
 
 def addEvents(size, difficulty):
-	fiftheenth = (1.0/150.0)
-	twelveth = (1.0/120.0)
-	tenth = (1.0/100.0)
-	
-	# add enemy events
-	if random.random() < fiftheenth * difficulty:
-		type = random.randint(1, 10)
-		if len(enemySpawn[EVENT_SINE]) == 0 and type <= 6:
-			addEventSinewave(size)
-		elif len(enemySpawn[EVENT_GRUNT]) == 0 and type > 6:
-			addEventVgrunts(size)
-	
-	# add randomly placed fodders
-	times = 0
-	for i in range(16):
-		if random.random() < twelveth * difficulty:
-			times += 1
-	for j in range(times):
-		addEventFodder(size)
-	
-	# add randomly placed sentries
-	if random.random() < fiftheenth * difficulty:
-		addEventSentry(size)
-	
-	# add randomly placed scopes, ASes and HPups
-	if random.random() < fiftheenth : addEventScope(size)
-	if random.random() < fiftheenth : addEventAS(size)
-	if random.random() < fiftheenth * difficulty: addEventHPup(size)
-	
+    twentieth = (1.0 / 200.0)
+    fiftheenth = (1.0 / 150.0)
+    twelveth = (1.0 / 120.0)
+    tenth = (1.0 / 100.0)
 
+    # add enemy events
+    if random.random() < fiftheenth * difficulty:
+        type = random.randint(1, 10)
+        if len(enemySpawn[EVENT_SINE]) == 0 and type <= 6:
+            addEventSinewave(size)
+        elif len(enemySpawn[EVENT_GRUNT]) == 0 and type > 6:
+            addEventVgrunts(size)
+
+    # add randomly placed fodders and grunts
+    timesFodder = timesGrunt = 0
+    for i in range(16):
+        if random.random() < twelveth * difficulty:
+            timesFodder += 1
+        if random.random() < twentieth * difficulty:
+            timesGrunt += 1
+    for j in range(0, timesFodder):
+        addEventFodder(size)
+    for j in range(0, timesFodder):
+        addEventGrunt(size)
+
+    # add randomly placed sentries
+    if random.random() < fiftheenth * difficulty:
+        addEventSentry(size)
+
+    # add randomly placed scopes, ASes and HPups
+    if random.random() < fiftheenth:
+        addEventScope(size)
+    if random.random() < fiftheenth:
+        addEventAS(size)
+    if random.random() < fiftheenth * difficulty:
+        addEventHPup(size)
