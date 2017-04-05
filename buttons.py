f29e0e6ebf0f5dd86bd3083fe2c8fc32c4585fc4
clocsinjr <clocsinjr@hotmail.com>
2017-04-05 13:56:32 +0200

    temporary texture for music toggle button

--- buttons.py
+++ buttons.py
@@ -3,6 +3,14 @@ import images
 
 import global_vars as g
 
+
+class gui_button():
+    def __init__(self, img, sel_img, func):
+        self.img = img
+        self.sel_img = sel_img
+        self.rect = img.get_rect()
+        self.func = func
+
 def b_continue():
     g.paused = not g.paused
 
@@ -15,17 +23,12 @@ def b_toggle_music():
     else:
         pygame.mixer.music.pause()
     g.music_paused = not g.music_paused
-        
-    
-class gui_button():
-    def __init__(self, img, sel_img, func):
-        self.img = img
-        self.sel_img = sel_img
-        self.rect = img.get_rect()
-        self.func = func
 
 
-button_continue = gui_button(images.button_continue, images.button_continue_sel, b_continue)
-button_exit = gui_button(images.button_exit, images.button_exit_sel, b_exit)
+
+button_continue = gui_button(
+    images.button_continue, images.button_continue_sel, b_continue)
+button_exit = gui_button(
+    images.button_exit, images.button_exit_sel, b_exit)
 button_toggle_music = gui_button(
-    images.button_exit, images.button_exit_sel, b_toggle_music)
+    images.button_music_toggle, images.button_music_toggle_sel, b_toggle_music)
--- images.py
+++ images.py
@@ -6,6 +6,8 @@ button_continue = pygame.image.load(src + "button_continue.png")
 button_continue_sel = pygame.image.load(src + "button_continue_sel.png")
 button_exit = pygame.image.load(src + "button_exit.png")
 button_exit_sel = pygame.image.load(src + "button_exit_sel.png")
+button_music_toggle = pygame.image.load(src + "button_toggle_music.png")
+button_music_toggle_sel = pygame.image.load(src + "button_toggle_music_sel.png")
 
 playerMoveNone = pygame.image.load(src + "playerNone.png")
 playerMoveLeft = pygame.image.load(src + "playerLeft.png")
